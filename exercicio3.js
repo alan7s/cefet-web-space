d46072c50dc20a4f4d0e26dff9b856f37244b315
alan7s <alan7@outlook.com>
2021-11-25 21:59:01 -0300

    Exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let allBotaoExpandeRetraiEl = document.querySelectorAll('.botao-expandir-retrair');
+
+for (let botaoExpandeRetraiEl of allBotaoExpandeRetraiEl) {
+    botaoExpandeRetraiEl.addEventListener('click', (e) => {
+        const el = e.currentTarget;
+        const btnEl = el.parentNode;
+        if (el.innerHTML === '+') {
+            el.innerHTML = '-';
+        } else {
+            el.innerHTML = '+';
+        }
+        btnEl.classList.toggle('expandido');
+    });
+}
